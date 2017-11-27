9a119a6dbeadd72879cefadf70311caf7607bd32
phinheng <phinheng.khmer@gmail.com>
2017-11-27 10:10:23 +0700

    sdf

--- www/PayPalSwiper.js
+++ www/PayPalSwiper.js
@@ -1,59 +1,11 @@
-var argscheck = require('cordova/argscheck'),
-channel = require('cordova/channel'),
-utils = require('cordova/utils'),
-exec = require('cordova/exec'),
-cordova = require('cordova');
-
-channel.createSticky('onCordovaInfoReady');
-// Tell cordova channel to wait on the CordovaInfoReady event
-channel.waitForInitialization('onCordovaInfoReady');
+var exec = require('cordova/exec');
 
 
-function PayPalSwiper() 
+function PayPalSwiper()
 {
-    this.available = false;
-    this.platform = null;
-    this.version = null;
-    this.uuid = null;
-    this.cordova = null;
-    this.model = null;
-    this.manufacturer = null;
-    this.isVirtual = null;
-    this.serial = null;
 
-    var me = this;
-
-    channel.onCordovaReady.subscribe(function() {
-        me.getInfo(function(info) {
-            //ignoring info.cordova returning from native, we should use value from cordova.version defined in cordova.js
-            //TODO: CB-5105 native implementations should not return info.cordova
-            var buildLabel = cordova.version;
-            me.available = true;
-            me.platform = info.platform;
-            me.version = info.version;
-            me.uuid = info.uuid;
-            me.cordova = buildLabel;
-            me.model = info.model;
-            me.isVirtual = info.isVirtual;
-            me.manufacturer = info.manufacturer || 'unknown';
-            me.serial = info.serial || 'unknown';
-            channel.onCordovaInfoReady.fire();
-        },function(e) {
-            me.available = false;
-            utils.alert("[ERROR] Error initializing Cordova: " + e);
-        });
-    });
+    this.heng = "Hello World!";
 }
 
-/**
- * Get device info
- *
- * @param {Function} successCallback The function to call when the heading data is available
- * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
- */
-PayPalSwiper.prototype.getInfo = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'PayPalSwiper.getInfo', arguments);
-    exec(successCallback, errorCallback, "PayPalSwiper", "getDeviceInfo", []);
-};
 
 module.exports = new PayPalSwiper();
